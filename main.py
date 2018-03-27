4cde91d6f2d69c7530be9dc733d718dc0a01e044
BuildTools <ckhawks@gmail.com>
2018-03-26 19:31:25 -0500

    added json usage, added readme stuff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json, requests
+import json, requests, sys
 
-print """
+print """  __    __
  /' \  /' \\
 /\_, \/\_, \\
 \/_/\ \/_/\ \\
@@ -11,11 +11,20 @@ print """
 
 print "--- 11's discord deleter thing ---"
 print "clears all of your messages in a channel"
-print "in order for this script to work properly the channel id, auth token, and username is required"
-username = raw_input("username: ")
-auth_token = raw_input("auth token: ")
-channel_id = raw_input("channel id: ")
-delete_from_all_users = True if raw_input("delete messages from other users (y/n): ") == "y" else False
+if(len(sys.argv) > 1):
+    if(sys.argv[1] == "config"):
+        with open('config.json') as json_data:
+            data = json.load(json_data)
+        username = data["username"]
+        auth_token = data["auth_token"]
+        channel_id = data["channel_id"]
+        delete_from_all_users = data["delete_from_all"]
+else:
+    print "in order for this script to work properly the channel id, auth token, and username is required"
+    username = raw_input("username: ")
+    auth_token = raw_input("auth token: ")
+    channel_id = raw_input("channel id: ")
+    delete_from_all_users = True if raw_input("delete messages from other users (y/n): ") == "y" else False
 
 def get_all_messages(auth, id, last="", prev=[]): # recursively find all messages in a channel, 100 at a time
     if not last: # first method call, start from beginning (might be able to remove)
