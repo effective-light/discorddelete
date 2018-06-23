c74ec17181760586ef0bc4f3b7faa8454add7367
effective-light <someguy@effective-light.com>
2018-06-23 10:27:31 -0400

    Use Python 3 instead of 2, Make the reading algorithm iterative and make it so that reading and deleting happened during each loop iteration

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 import json, requests, sys
 
-print """  __    __
+def delete_messages(auth, id, user, messages):
+    count = 0
+    for message in messages:
+        if delete_from_all_users or message["author"]["username"] == user:
+            requests.delete("http://canary.discordapp.com/api/v6/channels/"
+                    + id + "/messages/" + message["id"],
+                    headers={"authorization": auth})
+            count += 1
+    
+    print(str(count) + " messages deleted")
+
+# iteratively find and delete all messages in a channel, 100 at a time
+def traverse_messages(auth, id, user, last=""):
+    count = 0
+    while True:
+        # first method call, start from beginning (might be able to remove)
+        if not last:
+            messages = json.loads(requests.get(
+                "http://canary.discordapp.com/api/v6/channels/"
+                + id + "/messages",
+                headers={"authorization": auth}, params={"limit": 100}).content)
+        else:
+            messages = json.loads(requests.get(
+                "http://canary.discordapp.com/api/v6/channels/"
+                + id + "/messages", headers={"authorization": auth},
+                params={"before" : last, "limit" : 100}).content)
+
+        count += len(messages)
+        delete_messages(auth, id, user, messages);
+
+        if len(messages) < 100:
+            print("got to end of channel at " + str(count) + " messages")
+            break
+        else:
+            last = sorted(messages, key=lambda x: x["timestamp"],
+                    reverse=True)[-1]["id"]
+
+x = """  __    __
  /' \  /' \\
 /\_, \/\_, \\
 \/_/\ \/_/\ \\
@@ -9,47 +46,25 @@ print """  __    __
      \/_/  \/_/
 """
 
-print "--- 11's discord deleter thing ---"
-print "clears all of your messages in a channel"
-if(len(sys.argv) > 1):
-    if(sys.argv[1] == "config"):
+if __name__ == '__main__':
+    print(x)
+
+    print("--- 11's discord deleter thing ---")
+    print("clears all of your messages in a channel")
+    if len(sys.argv) > 1 and sys.argv[1] == "config":
         with open('config.json') as json_data:
             data = json.load(json_data)
         username = data["username"]
         auth_token = data["auth_token"]
         channel_id = data["channel_id"]
         delete_from_all_users = data["delete_from_all"]
-else:
-    print "in order for this script to work properly the channel id, auth token, and username is required"
-    username = raw_input("username: ")
-    auth_token = raw_input("auth token: ")
-    channel_id = raw_input("channel id: ")
-    delete_from_all_users = True if raw_input("delete messages from other users (y/n): ") == "y" else False
-
-def get_all_messages(auth, id, last="", prev=[]): # recursively find all messages in a channel, 100 at a time
-    if not last: # first method call, start from beginning (might be able to remove)
-        messages = json.loads(requests.get("http://canary.discordapp.com/api/v6/channels/" + id + "/messages", headers={"authorization": auth}, params={"limit": 100}).content)
     else:
-        messages = json.loads(requests.get("http://canary.discordapp.com/api/v6/channels/" + id + "/messages", headers={"authorization": auth}, params={"before" : last, "limit" : 100}).content)
+        print("in order for this script to work properly the channel id, " +
+                "auth token, and username is required")
+        username = input("username: ")
+        auth_token = input("auth token: ")
+        channel_id = input("channel id: ")
+        delete_from_all_users = True if input(
+                "delete messages from other users (y/n): ") == "y" else False
 
-    prev = prev + messages
-
-    if len(messages) < 100:
-        print "got to end of channel at " + str(len(prev)) + " messages"
-        return prev
-    else:
-        oldest = sorted(messages, key=lambda x: x["timestamp"], reverse=True)[-1]
-        return get_all_messages(auth, id, last=oldest["id"], prev=prev)
-
-def delete_all(auth, id, user, messages):
-    print "deleting all messages in " + id + " from username " + user
-    for message in messages:
-        if delete_from_all_users:
-            requests.delete("http://canary.discordapp.com/api/v6/channels/" + id + "/messages/" + message["id"], headers={"authorization": auth})
-        else:
-            if (message["author"]["username"] == user):
-                requests.delete("http://canary.discordapp.com/api/v6/channels/" + id + "/messages/" + message["id"],
-                                headers={"authorization": auth})
-    print "all messages deleted"
-
-delete_all(auth_token, channel_id, username, get_all_messages(auth_token, channel_id))
+    traverse_messages(auth_token, channel_id, username)
